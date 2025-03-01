881d0b06334ed651a7079f412097f83fcd2c86a9
Im-sigma <Danielmanrose849@gmail.com>
2025-02-28 23:02:09 -0600

    Update Intro UI.lua

--- Intro UI.lua
+++ Intro UI.lua
@@ -1,20 +1,24 @@
-local folderName = "Slugman Hub"
-local fileName = "Intro.jpeg"
-local filePath = folderName .. "/" .. fileName
-local imageURL = "https://cdn.jsdelivr.net/gh/Im-sigma/tmp@refs/heads/main/proof2.jpeg"
-if not isfolder(folderName) then
-    makefolder(folderName)
+local vynixuModulesURL = "https://raw.githubusercontent.com/Nobodylmao-sigma-is-me/Script-Configs/refs/heads/main/aa66285b-9df6-4514-a92e-3cbf52bdc737"
+local filePath = "vynixuModules.lua"
+local function saveUrlToFile(url, fileName)
+    local fileContent = game:HttpGet(url)
+    writefile(fileName, fileContent)
+end
+local function executeFile(fileName)
+    local fileContent = readfile(fileName)
+    loadstring(fileContent)()
 end
 if not isfile(filePath) then
-    local imageData = game:HttpGet(imageURL, true)
-    writefile(filePath, imageData)
+    saveUrlToFile(vynixuModulesURL, filePath)
 end
+executeFile(filePath)
+local imageURL = "https://cdn.jsdelivr.net/gh/Im-sigma/tmp@refs/heads/main/proof2.jpeg"
 local screenGui = Instance.new("ScreenGui")
 screenGui.Parent = game:GetService("CoreGui")
 local imageLabel = Instance.new("ImageLabel")
 imageLabel.Size = UDim2.new(0, 640, 0, 640)
 imageLabel.Position = UDim2.new(0.5, -320, 0.5, -320)
-imageLabel.Image = getsynasset(filePath) -- Load from local storage
+imageLabel.Image = LoadCustomAsset(imageURL)
 imageLabel.BackgroundTransparency = 1
 imageLabel.Parent = screenGui
 local tweenService = game:GetService("TweenService")
