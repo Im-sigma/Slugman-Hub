187f2345499a431a46b4717db7d9a24e203b4951
Im-sigma <Danielmanrose849@gmail.com>
2025-04-12 20:57:59 -0500

    Update Intro UI.lua

--- Intro UI.lua
+++ Intro UI.lua
@@ -12,7 +12,7 @@ if not isfile(filePath) then
     saveUrlToFile(vynixuModulesURL, filePath)
 end
 executeFile(filePath)
-local imageURL = "https://cdn.jsdelivr.net/gh/Im-sigma/tmp@refs/heads/main/proof2.jpeg"
+local imageURL = "https://yt3.ggpht.com/QF5B2maCw8-Nxv_XHmYl_V1zxt7MwD-29Mlo3zObY-RWmwRjn4sjfY3iCYMHUbMqS8xO47MAWdvrLw=s640-c-fcrop64=1,00000000ffffffff-rw-nd-v1"
 local screenGui = Instance.new("ScreenGui")
 screenGui.Parent = game:GetService("CoreGui")
 local imageLabel = Instance.new("ImageLabel")
